35613919d8fb83bf9f903eeeb7b80e6c01f8cf9f
AbdizamedAli <abdizamed89@gmail.com>
2022-02-03 02:26:25 +0000

    format code

--- Starting Room/Assets/Scripts/keyRayCast.cs
+++ Starting Room/Assets/Scripts/keyRayCast.cs
@@ -1,52 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace keySystem
 {
     public class keyRayCast : MonoBehaviour
     {
-    public GameObject key_object;
-    public bool action = false;
-    public bool open = false;
-    private keyitemController key_to_get;
-    public GameObject instruction;
-    
-    void Update() 
-    {
+        public GameObject key_object;
+        public bool trigger = false;
+        public bool picked_up = false;
+        private keyitemController key_to_get;
+        public GameObject instruction;
 
-        if (Input.GetKeyDown(KeyCode.E))
+        void Update()
         {
-            if (action && !open)
-            {
-                key_object.SetActive(false);
-                key_to_get.objectInteraction();
-                action = false;
-                open = true;
-                instruction.SetActive(false);
 
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (trigger && !picked_up)
+                {
+                    key_object.SetActive(false);
+                    key_to_get.objectInteraction();
+                    trigger = false;
+                    picked_up = true;
+                    instruction.SetActive(false);
+
+                }
             }
         }
-    }
 
 
-    void OnTriggerEnter(Collider collision) 
-    {
-        if (collision.transform.tag == "Player" && !open)
+        void OnTriggerEnter(Collider collision)
         {
-            action = true;
-            key_to_get = key_object.GetComponent<keyitemController>();
-            instruction.SetActive(true);
+            if (collision.transform.tag == "Player" && !picked_up)
+            {
+                trigger = true;
+                key_to_get = key_object.GetComponent<keyitemController>();
+                instruction.SetActive(true);
+            }
         }
-    }
 
-    void OnTriggerExit(Collider other) 
-    {
-        instruction.SetActive(false);
+        void OnTriggerExit(Collider other)
+        {
+            instruction.SetActive(false);
 
-        action = false;    
-    }
+            trigger = false;
+        }
 
 
 
