4a6450f2b902fe2e8e5a52862cba990c7a52ffee
AbdizamedAli <abdizamed89@gmail.com>
2022-02-03 18:02:31 +0000

    Make actual key random colour and set it

--- Starting Room/Assets/Scripts/keyitemController.cs
+++ Starting Room/Assets/Scripts/keyitemController.cs
@@ -4,9 +4,24 @@ namespace keySystem
 {
     public class keyitemController : MonoBehaviour
     {
-        [SerializeField] private bool key = false;
+        [SerializeField] public bool key = false;
 
         [SerializeField] private KeyInventory key_inventory = null;
+
+        public Color key_colour;
+        public Vector3 actual_key_location;
+
+        void Awake()
+        {
+            key_colour = new Color(Random.Range(0f, 1f),
+                                   Random.Range(0f, 1f),
+                                   Random.Range(0f, 1f)
+                                   );
+        }
+        void Start()
+        {
+            gameObject.GetComponent<Renderer>().material.color = key_colour;
+        }
 
         public void objectInteraction()
         {
