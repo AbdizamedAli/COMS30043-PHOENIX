ad8be2dc0330c8d518b198aa212b2b505bac20ee
FlameRobin <60356880+FlameRobin@users.noreply.github.com>
2022-02-14 14:00:41 +0000

    Finish Constructing Wire Puzzle
    Wire puzzle now works (although doesn't have art/solvable puzzle yet).
    
    Currently, the puzzle can be 'solved' by rotating every piece three times.
    
    Power panel must be opened with screwdriver before button can be pressed.

--- Outside/Assets/Scripts/selectionAndInteraction/SelectionManager.cs
+++ Outside/Assets/Scripts/selectionAndInteraction/SelectionManager.cs
@@ -9,15 +9,24 @@ namespace ObjectInteraction
         //[SerializeField] Material highlightMaterial;
         //[SerializeField] Camera camera;
         [SerializeField] GameObject instruction;
+        [SerializeField] GameObject rotateInstruction;
+        [SerializeField] GameObject pressInstruction;
+        [SerializeField] GameObject solvedMessage;
         [SerializeField] Inventory inventory;
         private bool isObjectSelected;
         private GameObject objectSelected;
+        private GameObject redButton;
 
         // Start is called before the first frame update
         void Start()
         {
+            rotateInstruction.SetActive(false);
             instruction.SetActive(false);
+            pressInstruction.SetActive(false);
+            redButton = GameObject.Find("BigRedButton");
+            redButton.SetActive(false);
             isObjectSelected = false;
+            solvedMessage.SetActive(false);
         }
         // Update is called once per frame
         void Update()
@@ -35,9 +44,23 @@ namespace ObjectInteraction
                     isObjectSelected = true;
                     objectSelected = selectionRenderer.gameObject;
                 }
+                else if (selectionRenderer != null && selectionRenderer.tag == "Rotatable")
+                {
+                    rotateInstruction.SetActive(true);
+                    isObjectSelected = true;
+                    objectSelected = selectionRenderer.gameObject;
+                }
+                else if (selectionRenderer != null && selectionRenderer.gameObject.name == "BigRedButton")
+                {
+                    pressInstruction.SetActive(true);
+                    isObjectSelected = true;
+                    objectSelected = selectionRenderer.gameObject;
+                }
                 else
                 {
                     instruction.SetActive(false);
+                    rotateInstruction.SetActive(false);
+                    pressInstruction.SetActive(false);
                     isObjectSelected = false;
                 }
             }
@@ -47,13 +70,42 @@ namespace ObjectInteraction
             {
                 if (isObjectSelected)
                 {
-                    if (objectSelected.name == "Screwdriver_Single")
+                    if (objectSelected.tag == "Selectable")
                     {
-                        inventory.has_screwdriver = true;
+                        if (objectSelected.name == "Screwdriver_Single")
+                        {
+                            inventory.has_screwdriver = true;
+                        }
+                        if (inventory.has_screwdriver)
+                        {
+                            hit.collider.gameObject.SetActive(false);
+                            if (objectSelected.name == "PowerPanelFront")
+                            {
+                                redButton.SetActive(true);
+                            }                            
+                        }
                     }
-                    if (inventory.has_screwdriver)
+                    else if (objectSelected.tag == "Rotatable")
                     {
-                        hit.collider.gameObject.SetActive(false);
+                        objectSelected.transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self);
+                        Debug.Log(objectSelected.transform.rotation.eulerAngles);
+                    }
+                    else if (objectSelected.name == "BigRedButton")
+                    {
+                        var correctSolution = true;
+                        foreach (Transform panel in GameObject.Find("WirePuzzle").transform)
+                        {
+                            if (panel.rotation.eulerAngles.y % 360 != 0)
+                            {
+                                correctSolution = false;
+                                Debug.Log("Incorrect");
+                            }
+                        }
+                        if (correctSolution == true)
+                        {
+                            solvedMessage.SetActive(true);
+                            Debug.Log("Correct");
+                        }
                     }
                 }
             }
