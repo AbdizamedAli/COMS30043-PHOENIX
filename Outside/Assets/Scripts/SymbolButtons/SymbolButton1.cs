662a5170b90603083a6d1250d638f6cb707f8723
zapydoodle <72476451+zapydoodle@users.noreply.github.com>
2022-02-20 00:37:04 +0000

    completed room4
    buttons to press are generated randomly, player must press them in specific order. Backgrounds of selected buttons change when inputted successfully

---
+++ Outside/Assets/Scripts/SymbolButtons/SymbolButton1.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SymbolButton1 : MonoBehaviour
+{
+   private bool collision=false;
+    private bool pressed=false;
+    private SymbolChecker SymbolChecker;
+    // Start is called before the first frame update
+    void Awake(){
+        SymbolChecker=GameObject.FindObjectOfType<SymbolChecker>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collision){
+            if(Input.GetKeyDown(KeyCode.E)){
+               if(pressed==false){
+                   pressed=true;
+               }
+               else if(pressed==true){
+                   pressed=false;
+               }
+               SymbolChecker.SymbolOnePressed(pressed);
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        collision = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        collision = false;
+    }
+}
