24aa47337dbf02cdad97e14eeadfd760d5bce278
nmsl6666 <lyutianyueba@outlook.com>
2022-04-04 02:43:50 +0100

    new

--- puzzleroom_road/My project/Assets/script/bridge.cs
+++ puzzleroom_road/My project/Assets/script/bridge.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class bridge : MonoBehaviour
 {
+    public GameObject m_bridge;
+    public GameObject m_button;
+    public bool flag = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (flag == true)
+        {
+            UpdateMove();
+        }
         
+    }
+    public void UpdateMove()
+    {
+        float rx = Mathf.Sin(Time.time) * Time.deltaTime;
+        m_bridge.transform.Translate(new Vector3(0, 0, 10*rx));
+    }
+    void OnTriggerEnter(Collider m_button)
+    {
+        flag = false;
+    }
+    void OnTriggerExit(Collider m_button)
+    {
+        flag = true;
     }
 }
--- puzzleroom_road/My project/Assets/script/move.cs
+++ puzzleroom_road/My project/Assets/script/move.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
+    public GameObject go_button;
+    public GameObject move_cy;
+    public bool flag = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,35 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(flag == true)
+        {
+            float movev = 0;
+            float moveh = 0;
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                movev += Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                movev -= Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                moveh -= Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                moveh += Time.deltaTime;
+            }
+            move_cy.transform.Translate(new Vector3(moveh, 0, movev));
+        }
+    }
+    void OnTriggerEnter(Collider go_button)
+    {
+        flag = true;
+    }
+    void OnTriggerExit(Collider go_button)
+    {
+        flag = false;
     }
 }
