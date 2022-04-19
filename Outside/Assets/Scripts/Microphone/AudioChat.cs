264701029bb8946b5d656f1abb75c5071d5b3205
Abdizamed <nj18503@bristol.ac.uk>
2022-04-19 10:36:24 +0100

    Added Proximity chat but it doesn't work on Firefox
    Added Proximity chat but it doesn't work on Firefox

--- Outside/Assets/Scripts/Microphone/AudioChat.cs
+++ Outside/Assets/Scripts/Microphone/AudioChat.cs
@@ -2,76 +2,63 @@ using UnityEngine;
 using Photon.Pun;
 using Peer;
 using System;
+using System.Collections.Generic;
 
 public class AudioChat : MonoBehaviourPunCallbacks
 {
-    private GameObject target_player = null;
-
-    private float min_distance = 1f;
-    private float max_distance = 20f;
+    private bool connected = false;
     private float max_volume = 1f;
 
 
     void Start()
     {
         if (!photonView.IsMine) return;
-        //PeerjsWrapper.Instance.code_stack += checkConected;
-        //checkConected(PeerjsWrapper.Instance.Connection);
+        PeerjsWrapper.Instance.code_stack += checkConected;
+        checkConected(PeerjsWrapper.Instance.Connection);
 
     }
 
     private void Update()
     {
-        //UpdateVolume();
+        UpdateVolume();
     }
     private float getVolume(float distance)
     {
         return Math.Min(max_volume, max_volume / distance);
+    }
+    private void checkConected(Code code)
+    {
+        if (code == Code.ConnectionOn)
+        {
+            connected = true;
+        }
     }
 
     private void UpdateVolume()
     {
         if (!photonView.IsMine) return;
 
-        if (target_player != null)
-        {
-            float distance = Vector3.Distance(transform.position, target_player.transform.position);
-            float new_volume = getVolume(distance);
-            if (0f <= new_volume && new_volume <= 1f)
-            {
-                PeerjsWrapper.Instance.sendVolume(new_volume);
-            }
-        }
-    }
-
-    private void checkConected(Code code)
-    {
-        if (code == Code.ConnectionOn)
-        {
-            setPlayer();
-
-        }
-        
-    }
-
-    private void setPlayer()
-    {
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
-        if (players.Length > 1)
+        if (players.Length == PhotonNetwork.CountOfPlayers)
         {
 
             foreach (GameObject player in players)
             {
                 if (!player.GetPhotonView().IsMine)
                 {
-                    target_player = player;
+                    float distance = Vector3.Distance(transform.position, player.transform.position);
+                    float new_volume = getVolume(distance);
+                    Debug.Log(connected + " " + new_volume);
+                    if (0f <= new_volume && new_volume <= 1f && connected == true)
+                    {
+                        string ID = player.GetComponent<ConnectMicrophone>().ID;
+                        PeerjsWrapper.Instance.sendVolume(new_volume, ID);
+
+                    }
                 }
             }
         }
-        else
-        {
-            Invoke(nameof(setPlayer), 1);
-        }
     }
+
 }
