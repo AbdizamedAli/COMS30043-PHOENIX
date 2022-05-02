12562d873aadc1af3635741c3376729c03aae752
zapydoodle <72476451+zapydoodle@users.noreply.github.com>
2022-05-02 15:20:06 +0100

    Maze improvements
    Removed teleport pads
    Connected left and right sections
    Green door now blocks passage to right section
    Yellow door now blocks exit
    Moved torches from bottom section up to top section, now when bottom torches are turned on/off top torches are turned on/off too. Top torches can't be pressed
    Some of maze need retexturing

--- Outside/Assets/Supercyan Character Pack Free Sample/Scripts/torchTrigger.cs
+++ Outside/Assets/Supercyan Character Pack Free Sample/Scripts/torchTrigger.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class torchTrigger : MonoBehaviour
+public class torchTrigger : MonoBehaviourPunCallbacks
 {
     private bool collision=false;
     private bool pressed=false;
     public Material TorchOn;
     public Material TorchOff;
     public GameObject Torch;
+    public GameObject TorchAbove;
 
     void Update()
     {
@@ -25,6 +27,7 @@ public class torchTrigger : MonoBehaviour
                     
                     
                     Torch.GetComponent<Renderer>().material = TorchOff;
+                    this.photonView.RPC("FlipTopTorch",RpcTarget.All,"on");
                     pressed = false;
                 }
                 
@@ -33,7 +36,7 @@ public class torchTrigger : MonoBehaviour
                     
                     
                     Torch.GetComponent<Renderer>().material = TorchOn;
-                    
+                    this.photonView.RPC("FlipTopTorch",RpcTarget.All,"off");
                     pressed = true;
                 }
             }
@@ -47,6 +50,17 @@ public class torchTrigger : MonoBehaviour
     void OnTriggerExit(Collider other)
     {
         collision = false;
+    }
+
+    [PunRPC]
+    void FlipTopTorch(string state){
+        if (state=="on"){
+            TorchAbove.GetComponent<Renderer>().material = TorchOn;
+        
+        }
+        else if(state =="off"){
+            TorchAbove.GetComponent<Renderer>().material = TorchOff;
+        }
     }
 }
 
