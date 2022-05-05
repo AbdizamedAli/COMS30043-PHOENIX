f0e92bfc90988dd52113652b8990a3876ef47fd7
Abdizamed <nj18503@bristol.ac.uk>
2022-05-05 14:09:52 +0100

    Select key colour from distinct list
    Added Key model

--- Outside/Assets/Scripts/keySystem/RandomColour.cs
+++ Outside/Assets/Scripts/keySystem/RandomColour.cs
@@ -9,12 +9,11 @@ public class RandomColour : MonoBehaviourPunCallbacks
 
     private void Awake()
     {
-        gameObject.GetComponent<Renderer>().material.color = GetColor(Random.Range(0,5));
+        gameObject.GetComponent<Renderer>().material.color = GetColor(Random.Range(0,10));
     }
 
     private Color GetColor(int c)
     {
-        Debug.Log(c);
         switch (c)
         {
             case 0: return Color.green;
@@ -22,6 +21,12 @@ public class RandomColour : MonoBehaviourPunCallbacks
             case 2: return Color.black;
             case 3: return Color.cyan;
             case 4: return Color.magenta;
+            case 5: return new Color(1f,0.65f,0f);
+            case 6: return new Color(0.9f, 0.9f, 0.98f);
+            case 7: return new Color(0.66f, 0.4f, 0.16f);
+            case 8: return new Color(0.5f, 0f, 0f);
+            case 9: return new Color(0f, 0.5f, 0.5f);
+
         }
         return Color.blue;
     }
