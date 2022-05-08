9be24c2442239cdcede5c48f1fe207ee920c2dd4
FlameRobin <60356880+FlameRobin@users.noreply.github.com>
2022-05-08 18:41:38 +0100

    Add second teleport for AI puzzle
    Added a second spawn point for AI Riddle Puzzle. Synced this with photon (using code from red door trigger)

--- Outside/Assets/script/MoveFromFacility/AIDoor.cs
+++ Outside/Assets/script/MoveFromFacility/AIDoor.cs
@@ -6,10 +6,13 @@ using Photon.Pun;
 public class AIDoor : MonoBehaviour
 {
     [SerializeField] private GameObject spawn_1;
+    [SerializeField] private GameObject spawn_2;
+    private bool spawn1 = true;
+    private bool spawn2 = true;
     public bool isDone = false;
     [SerializeField] CursorControl cursorControl;
     [SerializeField] GameObject canvasUI;
-    [SerializeField] NetworkManager networkManager;
+    //[SerializeField] NetworkManager networkManager;
     private bool inRoom = false;
     // Start is called before the first frame update
     void Start()
@@ -39,13 +42,44 @@ public class AIDoor : MonoBehaviour
     {
         if (!isDone)
         {
-            other.transform.position = spawn_1.transform.position;
+            //other.transform.position = spawn_1.transform.position;
             //canvas.worldCamera = other.GetComponent<Camera>();
+            if (spawn1)
+            {
+                this.GetComponent<PhotonView>().RPC("setSpawn", RpcTarget.All, 0);
+                other.transform.position = spawn_1.transform.position;
+                spawn1 = false;
+
+            }
+            else if (spawn2)
+            {
+                other.transform.position = spawn_2.transform.position;
+                spawn2 = false;
+                this.GetComponent<PhotonView>().RPC("setSpawn", RpcTarget.All, 1);
+
+            }
+            else
+            {
+                Debug.Log("Full");
+            }
             inRoom = true;
         }
         var canvas = canvasUI.GetComponent<Canvas>();
         var player = other.gameObject;
         var camera = player.GetComponentInChildren<Camera>();
         canvas.worldCamera = camera;
+    }
+
+    [PunRPC]
+    private void setSpawn(int s)
+    {
+        if (s == 0)
+        {
+            spawn1 = false;
+        }
+        else
+        {
+            spawn2 = false;
+        }
     }
 }
