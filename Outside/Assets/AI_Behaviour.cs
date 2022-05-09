e5a6bb1b77845c3ca7b996076e5451b476b7d5fc
FlameRobin <60356880+FlameRobin@users.noreply.github.com>
2022-05-09 01:17:34 +0100

    Added Photon.Pun to AIPuzzle
    Added pun rpc calls to AI_Behaviour script

--- Outside/Assets/AI_Behaviour.cs
+++ Outside/Assets/AI_Behaviour.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
+using Photon.Pun;
 
 /// <summary>
 /// This class is used to control the behavior of our Robot by calling the HuggingFaceAPI instance.
